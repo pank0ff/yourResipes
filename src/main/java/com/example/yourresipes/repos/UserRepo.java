91b45390bebb1d5c9407fc305b6ed48ae5c9dd9e
pank0ff <scorpion-18krek@mail.ru>
2021-10-31 15:40:31 +0300

    ADD reg ang log form ADD DB ADD list of messages

---
+++ src/main/java/com/example/yourresipes/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.yourresipes.repos;
+
+import com.example.yourresipes.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
