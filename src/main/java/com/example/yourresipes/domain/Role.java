c816b2167b02ea1118005e838f5498b9070e4e9c
pank0ff <90576203+pank0ff@users.noreply.github.com>
2021-11-11 19:24:51 +0300

    Revert "ADD roles and Admin with his own functional"
    This reverts commit 09cbeffaf888291d7b021222a9d5c37286e15d21.

--- src/main/java/com/example/yourresipes/domain/Role.java
+++ src/main/java/com/example/yourresipes/domain/Role.java
@@ -3,7 +3,7 @@ package com.example.yourresipes.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER, ADMIN;
+    USER;
 
     @Override
     public String getAuthority() {
