09cbeffaf888291d7b021222a9d5c37286e15d21
pank0ff <90576203+pank0ff@users.noreply.github.com>
2021-11-11 18:48:02 +0300

    ADD roles and Admin with his own functional ADD linking files to a message

--- src/main/java/com/example/yourresipes/domain/Message.java
+++ src/main/java/com/example/yourresipes/domain/Message.java
@@ -11,31 +11,33 @@ public class Message {
     private String text;
     private String tag;
 
-
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
 
-    public  User getAuthor(){
-        return author;
-    }
+    private String filename;
 
     public Message() {
+    }
+
+    public Message(String text, String tag, User user) {
+        this.author = user;
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+    public User getAuthor() {
+        return author;
     }
 
     public void setAuthor(User author) {
         this.author = author;
     }
 
-    public Message(String text, String tag, User user) {
-        this.text = text;
-        this.tag = tag;
-        this.author = user;
-    }
-
-    public  String getAuthorName(){
-        return  author != null ? author.getUsername() : "<none>";
-    }
     public void setText(String text) {
         this.text = text;
     }
@@ -58,5 +60,13 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
